12d07486a9a86987d8310d0aa4873fbfafcc1ec1
AitBassouAli <aitbassouali@gmail.com>
2019-03-26 17:51:24 +0100

    import team data from the webservice

--- src/main/java/com/sqli/imputation/service/impl/DefaultTeamPopulatorService.java
+++ src/main/java/com/sqli/imputation/service/impl/DefaultTeamPopulatorService.java
@@ -1,8 +1,11 @@
 package com.sqli.imputation.service.impl;
 
+import com.sqli.imputation.domain.ProjectType;
 import com.sqli.imputation.domain.Team;
+import com.sqli.imputation.repository.ProjectTypeRepository;
 import com.sqli.imputation.service.TeamPopulatorService;
 import com.sqli.imputation.service.TeamService;
+import com.sqli.imputation.service.dto.ProjectTypeDTO;
 import com.sqli.imputation.service.dto.TeamDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +15,10 @@ public class DefaultTeamPopulatorService implements TeamPopulatorService {
 
     @Autowired
     private TeamService teamService;
+    @Autowired
+    private DefaultDbPopulator defaultDbPopulator;
+    @Autowired
+    private ProjectTypeRepository projectTypeRepository;
 
     @Override
     public Team populateDatabase(TeamDTO teamDTO) {
@@ -23,6 +30,16 @@ public class DefaultTeamPopulatorService implements TeamPopulatorService {
         Team team = new Team();
         team.setName(teamDTO.getLibelle());
         team.setAgresso(teamDTO.getAgresso());
+        team.setProjectType(findProjectType(teamDTO.getType()));
         return team;
+    }
+
+    private ProjectType findProjectType(String id) {
+        Long idDTO = Long.valueOf(Integer.parseInt(id));
+        return projectTypeRepository.findByNameLike(findProjectTypeByIdFromWs(idDTO).getLibelle());
+    }
+
+    private ProjectTypeDTO findProjectTypeByIdFromWs(Long id) {
+        return defaultDbPopulator.getProjectTypes().stream().filter(typeDTO -> typeDTO.getId() == id).findFirst().get();
     }
 }
