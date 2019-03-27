edcbc0e820378088ef0c29616333fc06552e354f
Ghassan <ghassanguessous1@gmail.com>
2019-03-27 15:28:43 +0100

    customising : imputation-type, project-type, team

--- src/main/webapp/app/shared/model/delivery-coordinator.model.ts
+++ src/main/webapp/app/shared/model/delivery-coordinator.model.ts
@@ -1,7 +1,10 @@
 export interface IDeliveryCoordinator {
     id?: number;
+    firstName?: string;
+    lastName?: string;
+    login?: string;
 }
 
 export class DeliveryCoordinator implements IDeliveryCoordinator {
-    constructor(public id?: number) {}
+    constructor(public id?: number, public firstName?: string, public lastName?: string, public login?: string) {}
 }
