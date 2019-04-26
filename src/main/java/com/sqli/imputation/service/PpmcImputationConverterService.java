2518e5a175e6d4f26c9a7cdbc6b81e8577ee4fda
Ghassan <ghassanguessous1@gmail.com>
2019-04-26 16:48:45 +0100

    get ppmc imputations from excel/DB

--- src/main/java/com/sqli/imputation/service/PpmcImputationConverterService.java
+++ src/main/java/com/sqli/imputation/service/PpmcImputationConverterService.java
@@ -1,11 +1,12 @@
 package com.sqli.imputation.service;
 
 import com.sqli.imputation.domain.Imputation;
+import com.sqli.imputation.domain.Team;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Optional;
 
 public interface PpmcImputationConverterService {
 
-    Optional<Imputation> getPpmcImputationFromExcelFile(MultipartFile file);
+    Optional<Imputation> getPpmcImputationFromExcelFile(MultipartFile file, Team team);
 }
